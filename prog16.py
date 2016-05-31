c7532ca00c1926235dc6466236a6eb43f2325e91
yur <yurcapricorn@gmail.com>
2016-05-31 21:11:17 +0300

    16py

---
+++ prog16.py
@@ -0,0 +1,26 @@
+#Sample Input:
+# a3b4c2e10b1
+#Sample Output:
+# aaabbbbcceeeeeeeeeeb
+
+with open('input.txt', 'r') as input:
+    s1=input.readline().strip()
+
+#s1='a3b4c2e10b1'
+s2=''
+nr=''
+sym=''
+for i in range(len(s1)):
+	if s1[i] in ('1234567890'):
+	    nr+=s1[i]
+	    if i==len(s1)-1:
+	        s2+=sym*int(nr)
+	else:
+	    if len(nr)>0:
+	        s2+=sym*int(nr)
+	        nr=''	       
+	    sym=s1[i]
+with open('output.txt', 'w') as output:
+    output.write(s2)
+		
+		
